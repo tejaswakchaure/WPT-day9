98d08bf764eb0291284ef7d3c12143d6aa1cd5a5
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-09 21:35:54 +0530

    internal module

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-import { sum } from "./calc.js"
+// how to read a file using NodeJS
 
+/*import { sum } from "./calc.js"   // ./=> using local module ,program created by me
 let op = sum(100,100);
 
-console.log("sum = "+op);
+console.log("sum = "+op);*/
+
+console.log("**********************************************");
+// this is a internal module.
+import {readFileSync} from "node:fs";
+
+let filePath = "D:\CDAC\WPT santosh sir\day9\classWork\src\calc.js";
+let filedata = readFileSync(filePath,{encoding:"utf-8"});
+console.log(filedata);
